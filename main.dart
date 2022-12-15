3f990a6f5fd61ddf367e08cc29b68e5ddf1af88f
LeoMhango <leoflightmhango@gmail.com>
2022-12-15 10:08:30 +0200

    rename method

--- main.dart
+++ main.dart
@@ -1,18 +1,9 @@
 import 'dart:io';
 
 main() async {
-  var file = File('data.txt');
-  var contents;
-
-  if (await file.exists()) {
-    contents = await file.readAsString();
-    print(contents);
-
-    var fileCopy = await File('data-copy.txt').writeAsString(contents);
-
-    print(await fileCopy.exists());
-
-    print(await fileCopy.length());
+  File myfile = File('myfile.txt');
+  if (await myfile.exists()) {
+    myfile.rename('yourFile.txt').then((_) => print('file renamed'));
   } else {
     print("file doesn't exist");
   }
