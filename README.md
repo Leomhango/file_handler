27a49c0c96cb3e0e2f5d8a56b65de4fa0f6bb06d
LeoMhango <leoflightmhango@gmail.com>
2022-12-22 14:46:41 +0200

    i think this is the last commit from this code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # file_handler
+
+this didn't work that well coz it didn't have pubsec.yml
--- main.dart
+++ main.dart
@@ -1,45 +1,70 @@
 import 'dart:io';
 
-createDirectory() async {
-  var videos = Directory("videos").create();
-  var music = Directory("music").create();
-  var documents = Directory("documents").create();
-  var image = Directory("images").create();
-}
+// createDirectory() async {
+//   var videos = Directory("videos").create();
+//   var music = Directory("music").create();
+//   var documents = Directory("documents").create();
+//   var image = Directory("images").create();
+//   // var temp = Directory("temp").create();
+// }
 
-moveFiles(var files, var dirList) async {
-  await for (final FileSystemEntity f in dirList) {
-    if (f is File) {
-      files = f.path;
+// checkDirs() async {
+//   if (!Directory("videos").existsSync() &&
+//       !Directory("music").existsSync() &&
+//       !Directory("documents").existsSync() &&
+//       !Directory("images").existsSync()) {
+//     createDirectory();
+//   }
+// }
 
-      String results = files.replaceAll(".\\", "");
+// moveFiles(var files, var dirList) async {
+//   await for (final FileSystemEntity f in dirList) {
+//     if (f is File) {
 
-      var myfile = File(results);
+//       files = f.path;
 
-      String text = myfile.toString();
-      String firstStage = text.replaceAll("File: '", "");
-      String secondStage = firstStage.replaceAll("'", "");
+//       var baseName = Path;
 
-      if (secondStage != 'main.dart' && secondStage != 'README.md') {
-        print(secondStage);
-        myfile.rename('temp/$secondStage').then((_) => print("done"));
-      } else {
-        print("no files");
-      }
-    }
+//       String results = files.replaceAll(".\\", "");
+
+//       var myfile = File(results);
+
+//       String text = myfile.toString();
+//       String firstStage = text.replaceAll("File: '", "");
+//       String secondStage = firstStage.replaceAll("'", "");
+
+//       if (secondStage != 'main.dart' && secondStage != 'README.md') {
+//         print(secondStage);
+//         myfile.rename('temp/$secondStage').then((_) => print("done"));
+//       } else {
+//         print("no files");
+//       }
+//     }
+//   }
+// }
+
+// readFiles() async {
+//   var dir = Directory("");
+//   var files;
+
+//   var dirList = dir.list(recursive: false);
+
+//   moveFiles(files, dirList);
+// }
+
+getFileList() {
+  var dir = Directory('./');
+  var files = dir.listSync(followLinks: false).whereType<File>();
+
+  for (var file in files) {
+    print('${file.path}: ${Path(file.path).extension}');
   }
 }
 
-readFiles() async {
-  var dir = Directory("");
-  var files;
-
-  var dirList = dir.list(recursive: false);
-
-  moveFiles(files, dirList);
-}
+checkFileExt() {}
 
 main() async {
-  readFiles();
-  createDirectory();
+  // checkDirs();
+  // readFiles();
+  getFileList();
 }
